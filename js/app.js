35877eb4d502d1d7af5b220e9a6a6a37563fe679
mithon <mithonphilip@gmail.com>
2023-10-28 03:47:30 +0600

    counter responsive design and javascript core counter funcation all complated.

--- js/app.js
+++ js/app.js
@@ -12,3 +12,27 @@
 //   toggleOpenBtn.style.display = "block";
 //   console.log("close button");
 // });
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    function counter(id, start, end, duration) {
+        let obj = document.getElementById(id),
+            current = start,
+            range = end - start,
+            increment = end > start ? 1 : -1,
+            step = Math.abs(Math.floor(duration / range)),
+            timer = setInterval(() => {
+                current += increment;
+                obj.textContent = current;
+                if (current == end) {
+                    clearInterval(timer);
+                }
+            }, step);
+    }
+    counter("countOne", 0, 400, 3000);
+    counter("countTow", 100, 300, 2500);
+    counter("countThree", 0, 250, 3000);
+    counter("countFour", 0, 120, 3000);
+});
