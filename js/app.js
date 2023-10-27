ef4dfa4d0a780c9a962a8f23e099f62ea229322b
mithon <mithonphilip@gmail.com>
2023-10-27 16:03:59 +0600

    responsive footer

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// const toggleOpenBtn = document.getElementById("toggleOpenBtn");
+// const toggleCloseBtn = document.getElementById("toggleCloseBtn");
+
+// toggleOpenBtn.addEventListener("click", function () {
+//   toggleOpenBtn.style.display = "none";
+//   toggleCloseBtn.style.display = "block";
+//   console.log("open button");
+// });
+
+// toggleCloseBtn.addEventListener("click", function () {
+//   toggleCloseBtn.style.display = "none";
+//   toggleOpenBtn.style.display = "block";
+//   console.log("close button");
+// });
